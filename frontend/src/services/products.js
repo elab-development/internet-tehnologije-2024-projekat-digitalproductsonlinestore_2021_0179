43a361818ba6d4c9d13a878ddb3f1477cf3adec4
MagdalenaB0707 <megibudimir@gmail.com>
2025-06-09 22:35:17 +0200

    resetovan frontend

--- frontend/src/services/products.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-
-export const getAllProducts = async () => {
-  const response = await api.get('/products');
-  return response.data.data || []; // <<< uzmi samo deo sa nizom proizvoda
-};
-
